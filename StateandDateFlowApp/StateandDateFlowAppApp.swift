dcb2233942dde731d55484662645dad867eab7b2
Albina <olifirenko.albina@gmail.com>
2022-10-26 13:05:09 +0300

    Initial Commit

---
+++ StateandDateFlowApp/StateandDateFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateandDateFlowAppApp.swift
+//  StateandDateFlowApp
+//
+//  Created by mac on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct StateandDateFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
